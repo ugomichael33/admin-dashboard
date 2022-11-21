c879460f6b1c6aabcf51f582a633a64c0065dc5f
ugochukwu onyekanma <ugochikanele@gmail.com>
2022-11-21 13:02:58 +0100

    added more pages

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -77,7 +77,32 @@ const Team = () => {
         <Box display='flex' justifyContent='space-between' alignItems='center'>
           <Header title='Team' subtitle='Managing the team members' />
         </Box>
-        <Box m='40px 0 0 0' height='75vh'>
+        <Box 
+            m='40px 0 0 0' 
+            height='75vh'
+            sx={{
+                "& .MuiDataGrid-root": {
+                    border: "none",
+                },
+                "& .MuiDataGrid-cell": {
+                    borderBottom: "none",
+                },
+                "& .name-column--cell": {
+                    color: colors.greenAccent[300]
+                },
+                "& .MuiDataGrid-columnHeaders": {
+                    backgroundColor: colors.blueAccent[700],
+                    borderBottom: "none"
+                },
+                "& .MuiDataGrid-virtualScroller": {
+                    backgroundColor: colors.primary[400]
+                },
+                "& .MuiDataGrid-footerContainer": {
+                    borderTop: "none",
+                    backgroundColor: colors.blueAccent[700]
+                }
+            }}
+        >
             <DataGrid 
                 rows={mockDataTeam}
                 columns={columns}
